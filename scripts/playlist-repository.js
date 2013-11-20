1cd04197a97d7324ef1f28a63f22a5eb41e64aba
Ben D'Herville <ben.dherville@gmail.com>
2013-11-21 00:14:46 +1100

    Images and refactoring.

--- scripts/playlist-repository.js
+++ scripts/playlist-repository.js
@@ -9,51 +9,105 @@ require([
         return xmlText.replace(/(&(?!amp;))/g, "&amp;");
     }
 
+    function parsePlaylistTracksFromAmrapResponse(xml) {
+        var playlistTracks = [];
+        xml.find(".playlist-track").children().not(".trackId").not(".noTrackContent").each(function () {
+            var artistAndTrack = $(this).text().split("-", 2);
+            var artistName = artistAndTrack[0].trim();
+            var trackName = artistAndTrack[1].trim();
+            console.log('Artist: ' + artistName + ', Track: ' + trackName);
+            playlistTracks.push({ "artistName": artistName, "trackName": trackName})
+        });
+        return playlistTracks;
+    }
+
+    function parallelizeTasks(arr, fn, done) {
+        var total = arr.length,
+            doneTask = function () {
+                if (--total === 0) {
+                    done();
+                }
+            };
+
+        arr.forEach(function (value) {
+            fn(value, doneTask);
+        });
+    }
+
+    function isArtistNameInArtists(artistName, artistArray) {
+        var artistMatch = false;
+        for (var idx = 0; idx < artistArray.length; idx++) {
+            if (artistArray[idx].name.toLowerCase() == artistName.toLowerCase()) {
+                artistMatch = true;
+                break;
+            } else {
+                console.log('Artist did not match. Expected [' + artistName + '] but found [' + artistArray[idx].name + ']');
+            }
+        }
+        return artistMatch;
+    }
+
+    function findFirstSpotifyMatch(playlistTrack, callback) {
+        var searchResults = search.Search.search('artist:"' + playlistTrack.artistName + '"+track:"' + playlistTrack.trackName + '"');
+
+        searchResults.tracks.snapshot(0, 1).done(function (snapshot) {
+            if (snapshot.length > 0) {
+                snapshot.loadAll('name').done(function (tracks) {
+                    tracks.forEach(function (track) {
+                        console.log(track.name, track.uri);
+
+                        if (isArtistNameInArtists(playlistTrack.artistName, track.artists)) {
+                            callback(track);
+                        } else {
+                            callback();
+                        }
+                    });
+                });
+            } else {
+                callback();
+            }
+        });
+    }
+
+    function displayPlaylist(loadedPlaylist) {
+        var list = List.forCollection(loadedPlaylist.tracks.sort('name'));
+        document.getElementById('playlistContainer1').appendChild(list.node);
+        list.init();
+    }
+
     var showPlaylist = function () {
         var xmlHttpRequest = new XMLHttpRequest()
         xmlHttpRequest.open('GET', 'http://airnet.org.au/program/javascriptEmbed.php?episode=58447&helperStart=http%3A%2F%2Fwww.rrr.org.au&view=3', true);
         xmlHttpRequest.send();
         xmlHttpRequest.onreadystatechange = function () {
             if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
-
                 models.Playlist.createTemporary("Triple R").done(function (playlist) {
                     playlist.load("tracks").done(function (loadedPlaylist) {
+                        loadedPlaylist.tracks.snapshot().done(function (tracksSnapshot) {
+                            if (tracksSnapshot.length == 0) {
+                                var $xml = $(new DOMParser().parseFromString(sanitiseXmlString(xmlHttpRequest.responseText), 'text/xml'));
 
-                        var $xml = $(new DOMParser().parseFromString(sanitiseXmlString(xmlHttpRequest.responseText), 'text/xml'));
+                                var playlistTracks = parsePlaylistTracksFromAmrapResponse($xml);
 
-                        $xml.find(".playlist-track").children().not(".trackId").not(".noTrackContent").each(function () {
-
-                            var artistAndTrack = $(this).text().split("-", 2);
-                            var artistName = artistAndTrack[0].trim();
-                            var trackName = artistAndTrack[1].trim();
-                            console.log('Artist: ' + artistName + ', Track: ' + trackName);
-
-                            var searchResults = search.Search.search('artist:"' + artistName + '"+track:"' + trackName + '"');
-                            searchResults.tracks.snapshot(0, 1).done(function (snapshot) {
-                                snapshot.loadAll('name').done(function (tracks) {
-                                    tracks.forEach(function (track) {
-                                        console.log(track.name, track.uri);
-
-                                        var artistMatch = false;
-                                        for (var idx = 0; idx < track.artists.length; idx++) {
-                                            if (track.artists[idx].name.toLowerCase() == artistName.toLowerCase()) {
-                                                artistMatch = true;
-                                                break;
+                                parallelizeTasks(playlistTracks,
+                                    function (playlistTrack, callback) {
+                                        findFirstSpotifyMatch(playlistTrack, function (track) {
+                                            if (track) {
+                                                loadedPlaylist.tracks.add(track);
                                             }
-                                        }
 
-                                        if (artistMatch) {
-                                            loadedPlaylist.tracks.add(track);
-                                        }
-                                    });
-                                });
-                            });
+                                            callback();
+                                        });
+                                    },
+                                    function () {
+                                        displayPlaylist(loadedPlaylist);
+                                    }
+                                );
+                            } else {
+                                displayPlaylist(loadedPlaylist);
+                            }
                         });
                     });
-
-                    var list = List.forPlaylist(playlist);
-                    document.getElementById('playlistContainer1').appendChild(list.node);
-                    list.init();
                 });
             }
         }
